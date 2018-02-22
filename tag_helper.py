37d23348aba44e9e3eb1d0b76ce3e88381e27566
Walter Bormans <bormanwa@ie.ibm.com>
2018-02-22 21:25:29 +0000

    Simplyfies the tag searching functions

--- tag_helper.py
+++ tag_helper.py
@@ -6,148 +6,57 @@
 
 tag_limit = 25
 
+# Get the list from a text file.
+def get_list(list_url):
+    if isinstance(list_url, str):
+        full_list = open(list_url, 'r').read()
+        split_list = full_list.split('|')
+        finished_list = list(filter(None, split_list))
+        return finished_list
 
-# A bunch of methods that prepare lists
-def get_genders():
-    full_list = open('tag/gender.txt', 'r').read()
-    split_list = full_list.split('|')
-    finished_list = list(filter(None, split_list))
-    return finished_list
-
-
-def get_pairings():
-    full_list = open('tag/pairings.txt', 'r').read()
-    split_list = full_list.split('|')
-    finished_list = list(filter(None, split_list))
-    return finished_list
-
-
-def get_body():
-    full_list = open('tag/body.txt', 'r').read()
-    split_list = full_list.split('|')
-    finished_list = list(filter(None, split_list))
-    return finished_list
-
-
-def get_acts():
-    full_list = open('tag/sex_acts.txt', 'r').read()
-    split_list = full_list.split('|')
-    finished_list = list(filter(None, split_list))
-    return finished_list
-
-
-def get_positions():
-    full_list = open('tag/positions.txt', 'r').read()
-    split_list = full_list.split('|')
-    finished_list = list(filter(None, split_list))
-    return finished_list
-
-
-def get_fetishes():
-    full_list = open('tag/fetishes.txt', 'r').read()
-    split_list = full_list.split('|')
-    finished_list = list(filter(None, split_list))
-    return finished_list
-
-
-# A bunch of methods that find tags
-def find_gender(tags):
+# Sort tags into categories, based on provided categories.
+def find_tags(tags, search_list):
     tag_list = list()
     i = 0
     while i < len(tags):
-        if tags[i] in gender_list:
+        if tags[i] in search_list:
             tag_list.append(tags[i])
         i += 1
     return tag_list
 
-
-def find_fetish(tags):
+def find_other_tags(tags, search_list):
     tag_list = list()
     i = 0
     while i < len(tags):
-        if tags[i] in fetish_list:
+        if tags[i] not in search_list:
             tag_list.append(tags[i])
         i += 1
+    
     return tag_list
-
-
-def find_pairing(tags):
-    tag_list = list()
-    i = 0
-    while i < len(tags):
-        if tags[i] in pairing_list:
-            tag_list.append(tags[i])
-        i += 1
-    return tag_list
-
-
-def find_body(tags):
-    tag_list = list()
-    i = 0
-    while i < len(tags):
-        if tags[i] in body_list:
-            tag_list.append(tags[i])
-        i += 1
-    return tag_list
-
-
-def find_act(tags):
-    tag_list = list()
-    i = 0
-    while i < len(tags):
-        if tags[i] in act_list:
-            tag_list.append(tags[i])
-        i += 1
-    return tag_list
-
-
-def find_position(tags):
-    tag_list = list()
-    i = 0
-    while i < len(tags):
-        if tags[i] in position_list:
-            tag_list.append(tags[i])
-        i += 1
-    return tag_list
-
-
-def find_others(tags):
-    tag_list = list()
-    i = 0
-    while i < len(tags):
-        if tags[i] not in full_tag_list:
-            tag_list.append(tags[i])
-        i += 1
-    return tag_list
-
 
 # Starts the search
 # Called by Furbot
 def start_searching(tags):
-    gender_tags = find_gender(tags)
-    fetish_tags = find_fetish(tags)
-    pairing_tags = find_pairing(tags)
-    body_tags = find_body(tags)
-    act_tags = find_act(tags)
-    position_tags = find_position(tags)
-    other_tags = find_others(tags)
+    # start by filling all the tag lists.
+    gender_tags = find_tags(tags, get_list('tag/gender.txt'))
+    fetish_tags = find_tags(tags, get_list('tag/fetishes.txt'))
+    pairing_tags = find_tags(tags, get_list('tag/pairings.txt'))
+    body_tags = find_tags(tags, get_list('tag/body.txt'))
+    act_tags = find_tags(tags, get_list('tag/sex_acts.txt'))
+    position_tags = find_tags(tags, get_list('tag/positions.txt'))
+    # If it was not caught before, it's not in a previous list.
+    other_tags = find_other_tags(tags, gender_tags + fetish_tags + pairing_tags + body_tags + act_tags + position_tags)
+    
     fixed_tag_list = gender_tags + fetish_tags + pairing_tags + body_tags + act_tags + position_tags + other_tags
-    tag_count = 0
-    short_list = list()
-    while tag_count < tag_limit:
-        short_list.append(fixed_tag_list[tag_count])
-        tag_count += 1
-    extra_tags = len(fixed_tag_list) - tag_count
-    if extra_tags == 1:
+
+    # Create the short list by slicing 
+    short_list = fixed_tag_list[:tag_limit]
+
+    #extra_tags = len(fixed_tag_list) - tag_count
+    if len(fixed_tag_list) == tag_limit + 1:
         short_list.append('**^^^^And ^^^^' + str(extra_tags) + ' ^^^^other ^^^^tag**')
-    if extra_tags > 1:
+    elif len(fixed_tag_list) > tag_limit + 1:
         short_list.append('**^^^^And ^^^^' + str(extra_tags) + ' ^^^^other ^^^^tags**')
     return short_list
 
-gender_list = get_genders()
-pairing_list = get_pairings()
-body_list = get_body()
-act_list = get_acts()
-position_list = get_positions()
-fetish_list = get_fetishes()
-full_tag_list = gender_list + pairing_list + body_list + act_list + position_list + fetish_list
+
